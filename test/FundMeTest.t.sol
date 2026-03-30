7888a1fc865118a5d5a31774ed5b99958c1d150c
0xEmmyb2 <cyubahiroemmy12@gmail.com>
2026-03-30 20:47:06 +0200

    Gas Optimization of the contracts

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -12,6 +12,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether; // 10e18
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         //us -> FundMeTest -> FundMe
@@ -72,8 +73,14 @@ contract FundMeTest is Test {
         uint256 startingFundMeBalance = address(fundMe).balance;  
 
         //Act
+        // uint256 gasStart = gasleft();
+        // vm.txGasPrice(GAS_PRICE);
         vm.prank(fundMe.getOwner());  
         fundMe.withdraw();
+
+        // uint256 gasEnd = gasleft();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log("Gas Used:", gasUsed);
 
         //assert
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
